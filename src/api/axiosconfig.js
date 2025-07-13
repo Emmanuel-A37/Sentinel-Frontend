f124cf0b016004c39589bec627771130ded30f7e
Emmanuel Akanji <akanjiemmanuel37@gmail.com>
2025-07-13 15:06:24 +0100

    change

--- src/api/axiosconfig.js
+++ src/api/axiosconfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
  
 
 const api = axios.create({
-  baseURL: process.env.BACKEND_URL,
+  baseURL: process.env.BACKEND_URL || "http://localhost:4000/api/v1",
   withCredentials : true
 });
 
